96b0945dc92d502f108c81b088d154da5b365ea7
Pete Alexander <peter.d.alexander23@gmail.com>
2021-06-20 19:29:30 +0100

    Add completed section 1.

---
+++ 1./1.2-hello_world/main.rs
@@ -0,0 +1,9 @@
+
+// Define the main function.
+// All code between the curly brackets is included
+// The fn keyword specifies a function.
+// The main function executes first in all Rust programs (like C).
+
+fn main() {
+    println!("Hello, world!");  // Adding a ! to a function turns it into a macro
+}
