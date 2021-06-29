80740c33a2dccd8d78a55c518ed086fa8b0cd226
Pete Alexander <peter.d.alexander23@gmail.com>
2021-06-29 14:42:54 +0100

    Add quit option to temp converter

--- 3/3.A-fahrenheit_converter/src/main.rs
+++ 3/3.A-fahrenheit_converter/src/main.rs
@@ -10,7 +10,7 @@ fn main() {
     
         // Read input from cli
         // Flush output buffer to output before new line
-        print!("Please enter the number you'd like to convert: ");
+        print!("Please enter the number you'd like to convert or enter q to quit: ");
         io::stdout()
             .flush()
             .expect("Could not flush the output buffer!");
@@ -19,6 +19,10 @@ fn main() {
         io::stdin()
             .read_line(&mut input)
             .expect("Failed to read line from terminal!");
+
+        if input.trim() == "q" || input.trim() == "Q" {
+            break;
+        }
 
         let output: f32 = match input.trim().parse() {
             Ok(num) => num,
