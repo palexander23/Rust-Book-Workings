05b36bf5403ac60a57eab57f9334cb0d39c14780
Pete Alexander <peter.d.alexander23@gmail.com>
2021-06-29 12:37:10 +0100

    Add Fahrenheit conversion demo program

---
+++ 3/3.A-fahrenheit_converter/src/main.rs
@@ -0,0 +1,38 @@
+use std::io::{self, Write};
+
+fn main() {
+    println!("Welcome to the Fahrenheit Converter!");
+
+    loop {
+    
+        // String to store input value
+        let mut input = String::new();
+    
+        // Read input from cli
+        // Flush output buffer to output before new line
+        print!("Please enter the number you'd like to convert: ");
+        io::stdout()
+            .flush()
+            .expect("Could not flush the output buffer!");
+        
+        // Read in number
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line from terminal!");
+
+        let output: f32 = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Could not parse number!");
+                continue;
+            }
+        };
+
+        // Perform conversion
+        let output = (output - 32.0) * (5.0 / 9.0);
+
+        // Output result
+        println!("{}°F == {:.2}°C", input.trim(), output);
+
+    }
+}
